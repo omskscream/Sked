f64bc5e2ce9cee50194a76a4ae32e2ecba484ff4
omskscream <golovkov93@gmail.com>
2015-02-06 01:06:41 +0600

    Add progress bar to main fragment. Make main activity default

--- app/src/main/java/com/omskscream/sked/fragments/MainFragment.java
+++ app/src/main/java/com/omskscream/sked/fragments/MainFragment.java
@@ -1,10 +1,13 @@
 package com.omskscream.sked.fragments;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.omskscream.sked.R;
 
@@ -13,10 +16,41 @@ import com.omskscream.sked.R;
  */
 public class MainFragment extends Fragment {
 
+    private ProgressBar mProgressBar;
+    private TextView mUpperView;
+    private TextView mDownView;
+    private Handler mHandler = new Handler();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mProgressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+        mUpperView = (TextView) rootView.findViewById(R.id.txtMainUp);
+        mDownView = (TextView) rootView.findViewById(R.id.txtMainDown);
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mProgressBar.setVisibility(View.VISIBLE);
+        mUpperView.setVisibility(View.GONE);
+        mDownView.setVisibility(View.GONE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+//                TODO: get info from DB and counters
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        //TODO: update information on screen
+                        mProgressBar.setVisibility(View.GONE);
+                        mUpperView.setVisibility(View.VISIBLE);
+                        mDownView.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }).start();
     }
 }
